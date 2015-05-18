73ce0538da72b8fbe73288a394d74433fcd33292
Eric Youngberg <eric@lmtlss.net>
2015-05-18 13:39:20 -0500

    Cleaned out old pre-built files. Add Nasa JML.d

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,1 @@
-(function(w,d) {
 
-  $(".nav-activate").click(function() {
-    $("header").toggleClass("active");
-  });
-
-  $(".tour-button").click(function() {
-    $(".tour").toggleClass("active"); 
-  });
-
-})(window, document);
